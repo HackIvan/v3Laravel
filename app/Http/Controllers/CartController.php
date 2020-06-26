b607fb10cf8645149ea15838f2692a105cf8f0b4
HackIvan <ibrahimivank@yahoo.com>
2020-06-26 23:58:55 +0000

    push all

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function add(Product $product){
+
+
+
+        \Cart::session(auth()->id())->add(array(
+            'id' => $product->id,
+            'name' => $product->name,
+            'price' => $product->price,
+            'quantity' => 1,
+            'attributes' => array(),
+            'associatedModel' => $product
+        ));
+
+        return redirect()->route('cart.index');
+    }
+
+    public function index (){
+
+       $cartItems = \Cart::session(auth()->id())->getContent();
+        return view ('cart.index', compact ('cartItems'));
+    }
+}
---
+++ resources/views/cart/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<h2>Cart </h2>
+
+
+
+<table class="table">
+    <thead>
+        <tr>
+            <th> Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($cartItems as $item)
+        <tr>
+            <td scope="row">{{ $item->name }}</td>
+            <td>{{ $item->price }}</td>
+            <td>{{ $item->quantity }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+
+@endsection
